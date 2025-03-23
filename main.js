b28b7f5cd0eb1fb4e32f6fcebc46ed7a92abd81b
ValerioSmit <Valeriosmit@gmail.com>
2025-03-23 20:17:02 +0100

    Fixed underline problem in the language archrive. Began making collapsible button for the documents page.

--- main.js
+++ main.js
@@ -40,3 +40,19 @@ prevButton.addEventListener('click', () => {
 
 showImage(currentIndex);
 startAutoSlide();
+
+//collapsible text
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
