da550a98dcf8f715fcfdd7d701f925a62f0f13da
ValerioSmit <Valeriosmit@gmail.com>
2025-03-24 22:33:35 +0100

    Finished documents.html, added css, and main.js

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 //collapsible text
-var coll = document.getElementsByClassName("collapsible");
-var i;
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
-}
-//end collapsible text
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+  //end collapsible text
 
 let currentIndex = 0;
 const images = document.querySelectorAll('.sliding-picture img');
