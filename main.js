8b9e32ea03ea6a1d60a6be10ba4c0bc6ee3c0cd8
ValerioSmit <Valeriosmit@gmail.com>
2025-03-24 18:41:31 +0100

    Moved collapsible button to top, else it would not work

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+//collapsible text
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
 let currentIndex = 0;
 const images = document.querySelectorAll('.sliding-picture img');
 const prevButton = document.querySelector('.prev-btn');
@@ -41,18 +57,3 @@ prevButton.addEventListener('click', () => {
 showImage(currentIndex);
 startAutoSlide();
 
-//collapsible text
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
-}
